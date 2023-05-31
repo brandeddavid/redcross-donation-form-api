a9ef5d5d8295779528aa838dbed254b5133de5f7
David Mwangi <david.mathenge98@gmail.com>
2023-05-31 14:27:58 +0300

    set up pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+	apps: [
+		{
+			script: "node index.js",
+			watch: ".",
+		},
+		{
+			script: "./service-worker/",
+			watch: ["./service-worker"],
+		},
+	],
+
+	deploy: {
+		production: {
+			key: "donation-form-ec2.pem",
+			user: "ubuntu",
+			host: "18.202.244.58",
+			ref: "origin/main",
+			repo: "git@github.com:brandeddavid/redcross-donation-form.git",
+			path: "/home/ubuntu/api/",
+			"pre-deploy-local": "",
+			"post-deploy":
+				"source ~/.nvm/nvm.sh && npm install && pm2 reload ecosystem.config.js --env production",
+			"pre-setup": "",
+			"ssh-options": "ForwardAgent=yes",
+		},
+	},
+};
